5473567588d5a7399da387df3630b2ea5033cbd6
Forrest Voight <forrest@forre.st>
2011-10-07 01:41:51 -0400

    refactored into separate DDS class

--- dds.py
+++ dds.py
@@ -52,9 +52,9 @@ _ddsc_lib.DDS_DomainParticipant_create_topic.errcheck = check_none
 _ddsc_lib.DDS_DomainParticipant_delete_topic.argtypes = [ctypes.POINTER(DDS_DomainParticipant), ctypes.POINTER(DDS_Topic)]
 _ddsc_lib.DDS_DomainParticipant_delete_topic.errcheck = check_code
 
-
 class DDS_DataWriter(ctypes.Structure):
     pass
+
 
 _ddsc_lib.DDS_Publisher_create_datawriter.argtypes = [ctypes.POINTER(DDS_Publisher), ctypes.POINTER(DDS_Topic), ctypes.c_void_p, ctypes.c_void_p, ctypes.c_ulong]
 _ddsc_lib.DDS_Publisher_create_datawriter.restype = ctypes.POINTER(DDS_DataWriter)
@@ -62,23 +62,24 @@ _ddsc_lib.DDS_Publisher_create_datawriter.errcheck = check_none
 
 _ddsc_lib.DDS_Publisher_delete_datawriter.argtypes = [ctypes.POINTER(DDS_Publisher), ctypes.POINTER(DDS_DataWriter)]
 _ddsc_lib.DDS_Publisher_delete_datawriter.errcheck = check_code
+
+
 DDS_STATUS_MASK_NONE = ctypes.c_ulong(0)
 
 
 class Topic(object):
-    def __init__(self, library, topic_name, type_name):
+    def __init__(self, library, topic_name, data_type):
         self._library = library
         self._topic_name = topic_name
-        self._type_name = type_name
-        del library, topic_name, type_name
+        self._data_type = data_type
+        del library, topic_name, data_type
         
-        assert self._library._get_type_name(self._type_name) == self._type_name
-        self._library._register_type(self._type_name, self._type_name)
+        self._data_type._TypeSupport_register_type(self._library._participant, self._data_type._TypeSupport_get_type_name())
         
         self._topic =  _ddsc_lib.DDS_DomainParticipant_create_topic(
             self._library._participant,
             self._topic_name,
-            self._type_name,
+            self._data_type._TypeSupport_get_type_name(),
             _ddsc_lib.DDS_TOPIC_QOS_DEFAULT,
             None,
             DDS_STATUS_MASK_NONE,
@@ -92,12 +93,12 @@ class Topic(object):
             DDS_STATUS_MASK_NONE,
         )
         
-        self._writer2 = self._library._dw_narrow(self._type_name, self._writer)
+        self._narrowed_writer = self._data_type._DataWriter_narrow(self._writer)
     
     def send(self, msg):
-        instance = self._library._create_data(self._type_name)
+        instance = self._data_type._TypeSupport_create_data()
         x = ctypes.create_string_buffer(struct.pack("<16sII", "", 16, 0))
-        self._library._dw_write(self._type_name, self._writer2, instance, x)
+        self._data_type._DataWriter_write(self._narrowed_writer, instance, x)
     
     def __del__(self, _ddsc_lib=_ddsc_lib):
         _ddsc_lib.DDS_Publisher_delete_datawriter(
@@ -110,14 +111,11 @@ class Topic(object):
             self._topic,
         )
 
-
-class Library(object):
-    def __init__(self, so_path):
-        self._lib = ctypes.CDLL(so_path)
-        
+class DDS(object):
+    def __init__(self, domain_id=0):
         self._participant = _ddsc_lib.DDS_DomainParticipantFactory_create_participant(
             _ddsc_lib.DDS_DomainParticipantFactory_get_instance(),
-            0,
+            domain_id,
             _ddsc_lib.DDS_PARTICIPANT_QOS_DEFAULT,
             None,
             DDS_STATUS_MASK_NONE,
@@ -130,55 +128,70 @@ class Library(object):
             DDS_STATUS_MASK_NONE,
         )
         
-        self._topics = {}
+        self._counter = itertools.count()
     
-    def _get_type_name(self, name):
-        f = getattr(self._lib, name + 'TypeSupport_get_type_name')
+    def _get_name(self):
+        return "pythondds%i" % (self._counter.next(),)
+    
+    def get_topic(self, topic_name, data_type):
+        # XXX
+        return Topic(self, topic_name, data_type)
+
+import itertools
+id_generator = itertools.count()
+
+class LibraryType(object):
+    def __init__(self, lib, name):
+        self._lib, self._name = lib, name
+        del lib, name
+    
+    def _TypeSupport_get_type_name(self):
+        f = getattr(self._lib, self._name + 'TypeSupport_get_type_name')
         f.argtypes = []
         f.restype = ctypes.c_char_p
         f.errcheck = check_none
         return f()
     
-    def _register_type(self, name, type_name):
-        f = getattr(self._lib, name + 'TypeSupport_register_type')
+    def _TypeSupport_register_type(self, participant, type_name):
+        f = getattr(self._lib, self._name + 'TypeSupport_register_type')
         f.argtypes = [ctypes.POINTER(DDS_DomainParticipant), ctypes.c_char_p]
         f.errcheck = check_code
-        f(self._participant, type_name)
+        f(participant, type_name)
     
-    def _dw_narrow(self, name, writer):
-        f = getattr(self._lib, name + 'DataWriter_narrow')
+    def _DataWriter_narrow(self, writer):
+        f = getattr(self._lib, self._name + 'DataWriter_narrow')
         f.argtypes = [ctypes.POINTER(DDS_DataWriter)]
         f.restype = ctypes.c_void_p
         f.errcheck = check_none
         return f(writer)
     
-    def _create_data(self, name):
-        f = getattr(self._lib, name + 'TypeSupport_create_data')
+    def _TypeSupport_create_data(self):
+        f = getattr(self._lib, self._name + 'TypeSupport_create_data')
         f.argtypes = []
         f.restype = ctypes.c_void_p
         f.errcheck = check_none
         return f()
     
-    def _ts_print_data(self, name, d):
-        f = getattr(self._lib, name + 'TypeSupport_print_data')
+    def _TypeSupport_print_data(self, d):
+        f = getattr(self._lib, self._name + 'TypeSupport_print_data')
         f.argtypes = [ctypes.c_void_p]
         f(d)
     
-    def _dw_write(self, name, writer, x, y):
-        f = getattr(self._lib, name + 'DataWriter_write')
+    def _DataWriter_write(self, writer, x, y):
+        f = getattr(self._lib, self._name + 'DataWriter_write')
         f.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p]
         f.errcheck = check_code
         return f(writer, x, y)
-    
-    def get_topic(self, topic_name, type_name):
-        # XXX
-        return Topic(self, topic_name, type_name)
-    
-    def __del__(self):
-        pass
 
+class Library(object):
+    def __init__(self, so_path):
+        self._lib = ctypes.CDLL(so_path)
+    
+    def __getattr__(self, attr):
+        return LibraryType(self._lib, attr)
 
-x = Library('../build/DDSMessages/libddsmessages2.so')
-t = x.get_topic('newtopic2', 'DepthMessage')
+d = DDS()
+l = Library('../build/DDSMessages/libddsmessages2.so')
+t = d.get_topic('newtopic2', l.DepthMessage)
 while True:
     t.send({})
