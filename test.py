646a0ff0b72a39b4d7c433b8af828cd8a8719d3e
Forrest Voight <forrest@forre.st>
2011-10-10 02:51:06 -0400

    made test more generic and extensible

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import traceback
+import random
 
 import dds
 
@@ -8,23 +9,25 @@ recv = ['send', 'recv'].index(sys.argv[1])
 
 d = dds.DDS()
 l = dds.Library('../build/DDSMessages/libddsmessages2.so')
-t = d.get_topic('newtopic2', l.DepthMessage)
+
+topics = []
+topics.append((d.get_topic('t1', l.State), lambda: dict(workername=str(random.randrange(2**10)), state=random.randrange(-2**31, 2**31), health=random.random())))
 
 if recv:
     while True:
         time.sleep(.01)
+        t, mf = random.choice(topics)
         try:
             msg = t.recv()
         except dds.Error, e:
             if e.message == 'no data':
                 continue
             raise
-        print "Received:", msg
+        print "Received %r on %s" % (msg, t._topic_name)
 else:
-    x = 1.
     while True:
-        x += 1.245
-        msg = dict(timestamp=int(x*100), depth=x, humidity=x+2, thermistertemp=x+3, humiditytemp=x+4)
-        print "Sending:", msg
+        t, mf = random.choice(topics)
+        msg = mf()
+        print "Sending %r on %s" % (msg, t._topic_name)
         t.send(msg)
-        time.sleep(1)
+        time.sleep(.5)
